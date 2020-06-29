f5519115b5a092be4b7f7f62ac037d194713bcb9
Syntax73 <45923733+Syntax73@users.noreply.github.com>
2020-06-29 16:28:17 -0300

    Added pagination in TeamController

--- backend/src/controllers/TeamController.js
+++ backend/src/controllers/TeamController.js
@@ -1,24 +1,27 @@
+const sequelize = require('../database');
 const Project = require('../models/Project');
+const { paginate } = require('../config/paginate');
 
 class TeamController {
   async index(req, res) {
     const { project_id: id } = req.params;
+    const { page } = req.query;
+    const limit = 3;
 
-    const project = await Project.findByPk(id, {
-      include: {
-        association: 'team',
-        attributes: ['avatar', 'name', 'email', 'role'],
-        through: {
-          attributes: [],
-        },
-      },
-    });
+    const project = await Project.findByPk(id);
 
     if (!project) {
       return res.status(404).json({ message: 'Projeto não encontrado' });
     }
 
-    return res.json(project.team);
+    // TODO paginação não funciona de maneira correta
+    const team = await project.getTeam({
+      ...paginate(page, limit),
+      attributes: ['id', 'avatar', 'name', 'email', 'role'],
+      joinTableAttributes: [],
+    });
+
+    return res.json(team);
   }
 }
 
