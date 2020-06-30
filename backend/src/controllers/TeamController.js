2c75a9f6778594b1b2e9dbeb1d3155b18bbb333f
Syntax73 <45923733+Syntax73@users.noreply.github.com>
2020-06-30 10:58:04 -0300

    Fixed query using raw SQL
    Now team can be listed with pagination

--- backend/src/controllers/TeamController.js
+++ backend/src/controllers/TeamController.js
@@ -1,25 +1,30 @@
 const sequelize = require('../database');
-const Project = require('../models/Project');
-const { paginate } = require('../config/paginate');
 
 class TeamController {
   async index(req, res) {
     const { project_id: id } = req.params;
     const { page } = req.query;
     const limit = 3;
+    const offset = page * limit - limit;
 
-    const project = await Project.findByPk(id);
+    const team = await sequelize.query(
+      `
+        SELECT COUNT(*) AS count FROM project_users WHERE project_id = :projectId GROUP BY project_users.project_id;
 
-    if (!project) {
-      return res.status(404).json({ message: 'Projeto não encontrado' });
+        SELECT users.id, users.name, users.avatar, users.email, users.role FROM project_users 
+          LEFT JOIN users ON users.id = project_users.user_id
+          WHERE project_id = :projectId OFFSET :offset FETCH FIRST :limit ROW ONLY;
+      `,
+      {
+        type: sequelize.QueryTypes.SELECT,
+        replacements: { projectId: id, limit, offset },
+        raw: true,
+      }
+    );
+
+    if (!team) {
+      return res.status(404).json({ message: 'Time não encontrado' });
     }
-
-    // TODO paginação não funciona de maneira correta
-    const team = await project.getTeam({
-      ...paginate(page, limit),
-      attributes: ['id', 'avatar', 'name', 'email', 'role'],
-      joinTableAttributes: [],
-    });
 
     return res.json(team);
   }
