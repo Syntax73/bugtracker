00b452c6b66c64c59635733c1835c1d13c938925
Syntax73 <45923733+Syntax73@users.noreply.github.com>
2020-07-01 09:27:52 -0300

    Now ListComments can load components

--- frontend/src/components/comments/ListComments.vue
+++ frontend/src/components/comments/ListComments.vue
@@ -3,8 +3,28 @@
 </template>
 
 <script>
+import { mapActions, mapState } from 'vuex';
+
 export default {
-  name: 'ListComments'
+  name: 'ListComments',
+  computed: {
+    ...mapState({
+      project: (state) => state.project.project,
+      issue: (state) => state.issue.issue
+    })
+  },
+  methods: {
+    ...mapActions('comments', ['getComments']),
+
+    loadComments() {
+      const idProject = this.project.id;
+      const idIssue = this.issue.id;
+      this.getComments({ idProject, idIssue });
+    }
+  },
+  mounted() {
+    this.loadComments();
+  }
 };
 </script>
 
