63f3ac2621f21b821b19e61dcff7e87cda9f63c6
Syntax73 <45923733+Syntax73@users.noreply.github.com>
2020-07-30 10:12:14 -0300

    Fixed issue dto

--- backend/src/dto/issue.js
+++ backend/src/dto/issue.js
@@ -3,7 +3,16 @@ const yup = require('yup');
 module.exports = yup.object().shape({
   title: yup.string().required(),
   description: yup.string().required(),
-  type: yup.string().required(),
-  priority: yup.string().required(),
-  severity: yup.string().required(),
+  type: yup
+    .string()
+    .matches(/(bug|feature|duplicate|documentation)/)
+    .required(),
+  priority: yup
+    .string()
+    .matches(/(low|medium|high)/)
+    .required(),
+  severity: yup
+    .string()
+    .matches(/(critical|major|moderate|minor|cosmect)/)
+    .required(),
 });
