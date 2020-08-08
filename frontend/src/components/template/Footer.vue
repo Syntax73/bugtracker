d8b7d5b787a6567087620f199745a13cbf4aea20
Syntax73 <45923733+Syntax73@users.noreply.github.com>
2020-08-08 09:30:35 -0300

    Now the year is displayed correctly

--- frontend/src/components/template/Footer.vue
+++ frontend/src/components/template/Footer.vue
@@ -1,12 +1,21 @@
 <template>
   <v-footer app dark>
-    <span>&copy; 2019</span>
+    &copy;
+    <span>{{ year }}</span>
   </v-footer>
 </template>
 
 <script>
 export default {
-  name: 'Footer'
+  name: 'Footer',
+  data() {
+    return {
+      year: null
+    };
+  },
+  mounted() {
+    this.year = new Date().getFullYear();
+  }
 };
 </script>
 
