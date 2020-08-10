3424e6bd431f8197ce07b32e76e48114ba22c9ff
Syntax73 <45923733+Syntax73@users.noreply.github.com>
2020-08-10 16:44:22 -0300

    Created pie chart

---
+++ frontend/src/components/charts/PieChart.vue
@@ -0,0 +1,32 @@
+<script>
+import { Pie } from 'vue-chartjs';
+
+export default {
+  extends: Pie,
+  props: {
+    chartData: Array,
+    options: Object,
+    chartColors: Object
+  },
+  mounted() {
+    const pieLable = this.chartData.map((d) => d.label);
+    const count = this.chartData.map((d) => d.total);
+
+    const { borderColor, backgroundColor } = this.chartColors;
+
+    this.renderChart(
+      {
+        labels: pieLable,
+        datasets: [
+          {
+            borderColor,
+            backgroundColor,
+            data: count
+          }
+        ]
+      },
+      this.options
+    );
+  }
+};
+</script>
