deb9ba4917e81beccb303fac4c3ec4e75578b909
Syntax73 <45923733+Syntax73@users.noreply.github.com>
2020-08-11 09:18:11 -0300

    Created new type of chart

---
+++ frontend/src/components/charts/BarChart.vue
@@ -0,0 +1,29 @@
+<script>
+import { Bar } from 'vue-chartjs';
+
+export default {
+  extends: Bar,
+  props: {
+    label: String,
+    chartData: Array,
+    options: Object
+  },
+  mounted() {
+    const dates = this.chartData.map((d) => d.date);
+    const totals = this.chartData.map((d) => d.total);
+
+    this.renderChart(
+      {
+        labels: dates,
+        datasets: [
+          {
+            label: this.label,
+            data: totals
+          }
+        ]
+      },
+      this.options
+    );
+  }
+};
+</script>
