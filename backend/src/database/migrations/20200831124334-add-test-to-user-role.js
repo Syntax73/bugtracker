6ff09b58a3ae71fa190b94b86e18a6289b581995
Syntax73 <45923733+Syntax73@users.noreply.github.com>
2020-08-31 10:50:00 -0300

    Added a new enum value on role column in users table

---
+++ backend/src/database/migrations/20200831124334-add-test-to-user-role.js
@@ -0,0 +1,25 @@
+const replaceEnum = require('sequelize-replace-enum-postgres').default;
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return replaceEnum({
+      queryInterface,
+      tableName: 'users',
+      columnName: 'role',
+      defaultValue: 'developer',
+      newValues: ['developer', 'test_lead', 'project_lead', 'admin', 'test'],
+      enumName: 'enum_users_role',
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return replaceEnum({
+      queryInterface,
+      tableName: 'users',
+      columnName: 'role',
+      defaultValue: 'developer',
+      newValues: ['developer', 'test_lead', 'project_lead', 'admin'],
+      enumName: 'enum_users_role',
+    });
+  },
+};
